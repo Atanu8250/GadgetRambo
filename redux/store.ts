a4d2e55ffab44133647f1cfbbaf78fa22a46ae66
Atanu8250 <atanu20uparsole@gmail.com>
2023-01-15 12:04:54 +0530

    redux and store.ts added

---
+++ redux/store.ts
@@ -0,0 +1,17 @@
+import { legacy_createStore, compose, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+})
+
+// we need this part to use compose enhancer in typescript
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+// ----
+
+const composeEnhancer:any = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = legacy_createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
