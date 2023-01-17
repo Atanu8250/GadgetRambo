b7b352fa42efbd09654a55a9033c952daf97e481
rajkumar-cmd <raymarhossor@gmail.com>
2023-01-17 21:31:55 +0530

    navbar upper

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  images:{
+    unoptimized:true,
+    remotePatterns:[
+      {
+        protocol:"http",
+        hostname:"i.ibb.co",
+        port:"",
+        pathname:"**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <ChakraProvider>
+    <Navbar/>
+    <Component {...pageProps} />
+    </ChakraProvider>
+    </>
+  )
 }
