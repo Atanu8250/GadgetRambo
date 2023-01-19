44f44ed641ed0385861439c4c292b57aafb052b8
Atanu8250 <atanu20uparsole@gmail.com>
2023-01-19 22:49:53 +0530

    day-3 done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Provider store={store}>
-      {/* <Navbar /> */}
+      <Navbar />
         <Component {...pageProps} />
       </Provider>
     </ChakraProvider>
