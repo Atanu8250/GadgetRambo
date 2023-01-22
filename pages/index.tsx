e24f374830bbe21b67b97603cc3b9ed49cd914cc
Sachin Mishrae <msachin786.pm@gmail.comm>
2023-01-22 15:02:04 +0530

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -199,7 +199,11 @@ export default function Home() {
                     return;
                   }
                   return (
-                    <Link style={{width:"16%"}} key={mobile.id} href={`products/mobiles/${mobile.id}`}>
+                    <Link
+                      style={{ width: "16%" }}
+                      key={mobile.id}
+                      href={`products/mobiles/${mobile.id}`}
+                    >
                       <NewsCard
                         title={mobile.modal}
                         banner={mobile.imgsrc}
@@ -225,40 +229,7 @@ export default function Home() {
                 }}
               >
                 {mobiles.map((mobile: any, id: number) => {
-                  if (id > 12 && id<19) {
-                    return (
-                      <Link
-                        style={{ width: "16%" }}
-                        key={mobile.id}
-                        href={`products/mobiles/${mobile.id}`}
-                      >
-                        <NewsCard
-                          title={mobile.modal}
-                          banner={mobile.imgsrc}
-                          titleSize={"13px"}
-                          cardWidth={"100%"}
-                          imgHeight={"100px"}
-                          titleAlign={true}
-                        />
-                      </Link>
-                    )
-                  };
-                })}
-              </div>
-              {/* upcoming phones section  */}
-              <p>Upcoming Phones</p>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "space-evenly",
-                  backgroundColor: "white",
-                  margin: ".4rem",
-                  borderRadius: ".5rem",
-                  boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
-                }}
-              >
-                {mobiles.map((mobile: any, id: number) => {
-                  if (id > 20 && id<27) {
+                  if (id > 12 && id < 19) {
                     return (
                       <Link
                         style={{ width: "16%" }}
@@ -278,26 +249,57 @@ export default function Home() {
                   }
                 })}
               </div>
-              {/* popular comparisons carousel  */}
-              {/* <Slider {...ComparisonCarouselSettings}>
-                {mobiles.map((mobile: any, id) => {
-                  if (id >= 20) {
-                    return;
+              {/* upcoming phones section  */}
+              <p>Upcoming Phones</p>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  backgroundColor: "white",
+                  margin: ".4rem",
+                  borderRadius: ".5rem",
+                  boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+                }}
+              >
+                {mobiles.map((mobile: any, id: number) => {
+                  if (id > 20 && id < 27) {
+                    return (
+                      <Link
+                        style={{ width: "16%" }}
+                        key={mobile.id}
+                        href={`products/mobiles/${mobile.id}`}
+                      >
+                        <NewsCard
+                          title={mobile.modal}
+                          banner={mobile.imgsrc}
+                          titleSize={"13px"}
+                          cardWidth={"100%"}
+                          imgHeight={"100px"}
+                          titleAlign={true}
+                        />
+                      </Link>
+                    );
                   }
-                  return (
-                    <NewsCard
-                      key={mobile.id}
-                      title={mobile.modal}
-                      banner={mobile.imgsrc}
-                      titleSize={"15px"}
-                      cardWidth={"16%"}
-                      imgHeight={"100px"}
-                      titleAlign={true}
-                    />
-                  );
                 })}
-              </Slider> */}
+              </div>
             </div>
+          </div>
+          <div className={styles.popular}>
+            {news.map((el: any, i: number) => {
+              if (i > 5) {
+                return (
+                  <Link key={el.id} href={`blogs/${el.id}`}>
+                    <NewsCard
+                      title={el.title}
+                      titleSize={"15px"}
+                      banner={el.banner}
+                      imgHeight={"170px"}
+                      imgWidth={"250px"}
+                    />
+                  </Link>
+                );
+              }
+            })}
           </div>
         </div>
         {/* right side bar  */}
