8e4bc307ab94d59359d0ccdb8fd36385147849e3
rajkumar-cmd <raymarhossor@gmail.com>
2023-03-05 18:16:23 +0530

    admin user Details

--- components/admin/Navbar.tsx
+++ components/admin/Navbar.tsx
@@ -35,9 +35,10 @@ import { IconType } from 'react-icons';
 import { ReactText } from 'react';
 import Link from 'next/link'
 import Router from 'next/router';
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector} from 'react-redux';
 import { setHideAdminPanel, setShowAdminPanel } from '@/redux/auth/auth.action';
 import { Dispatch } from 'redux';
+import { State } from "@/redux/store";
 
 interface LinkItemProps {
   name: string;
@@ -180,6 +181,8 @@ interface MobileProps extends FlexProps {
   onOpen: () => void;
 }
 const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  const { user }: any = useSelector((store: State) => store.authManager);
+
   return (
     <Flex
       ml={{ base: 0, md: 60 }}
@@ -221,21 +224,19 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
               transition="all 0.3s"
               _focus={{ boxShadow: 'none' }}>
               <HStack>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
+              <Avatar
+                    size="sm"
+                    src={
+                      user.photoURL ||
+                      "https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small/Basic_Ui__28186_29.jpg"
+                    }
                 />
                 <VStack
                   display={{ base: 'none', md: 'flex' }}
                   alignItems="flex-start"
                   spacing="1px"
                   ml="2">
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
+                  <Text fontSize="sm">{user.displayName || "Admin"}</Text>
                 </VStack>
                 <Box display={{ base: 'none', md: 'flex' }}>
                   <FiChevronDown />
@@ -245,9 +246,9 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
             <MenuList
               bg={useColorModeValue('white', 'gray.900')}
               borderColor={useColorModeValue('gray.200', 'gray.700')}>
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
+              <MenuItem onClick={()=>{
+                window.location.href="/";
+              }}>Go to home page</MenuItem>
               <MenuDivider />
               <MenuItem>Sign out</MenuItem>
             </MenuList>
